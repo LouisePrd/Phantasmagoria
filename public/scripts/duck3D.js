8b61861e3827a9c878816a5ad8f911277ab70ce7
LouisePrd <louise.peridy22@gmail.com>
2025-02-17 10:51:00 +0100

    fix dependencies

--- public/scripts/duck3D.js
+++ public/scripts/duck3D.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from "../node_modules/three/build/three.module.js ";
 import { GLTFLoader } from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 import { OBJLoader } from "../node_modules/three/examples/jsm/loaders/OBJLoader.js";
 
